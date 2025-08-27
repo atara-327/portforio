8e9c8c309512bcd5b3988e6ccfb2440f0561f571
atara-327 <atara327a@gmail.com>
2025-08-27 10:43:22 +0900

    faviconとかbuildの設定とか

--- src/App.vue
+++ src/App.vue
@@ -3,21 +3,21 @@
 <template>
   <div class="main container">
     <h1 class="my-5 text-center">atara's PORTFOLIO</h1>
-    <div class="container py-5 text-center border border-white rounded shadow main-content">
-      <ul>
-        <li><i class="bi bi-twitter-x"></i> <a href="https://x.com/atara_0327" target="_blank">@atara_0327</a></li>
-        <li><i class="bi bi-github"></i> <a href="https://github.com/atara-327" target="_blank">atara-327</a></li>
+    <div class="container py-5 text-center border border-white rounded shadow card">
+      <ul class="list-unstyled">
+        <li><i class="bi bi-twitter-x"></i> <a class="link-offset-1" href="https://x.com/atara_0327" target="_blank">@atara_0327</a></li>
+        <li><i class="bi bi-github"></i> <a class="link-offset-1" href="https://github.com/atara-327" target="_blank">atara-327</a></li>
       </ul>
     </div>
     <br />
     <ul>
-      <li><a href="https://sia-song.ataraman.com" target="_blank">依澄しあ 歌枠検索</a></li>
+      <li><a class="link-offset-1" href="https://sia-song.ataraman.com" target="_blank">依澄しあ 歌枠検索</a></li>
     </ul>
 </div>
 </template>
 
 <style scoped>
-.main-content {
-  background-color: rgb(35, 17, 41);
+.card {
+  background-color: rgb(67, 40, 116);
 }
 </style>
