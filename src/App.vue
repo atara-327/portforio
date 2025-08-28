86891c01f83c65b59b377df0d3626643bd604945
atara-327 <atara327a@gmail.com>
2025-08-28 14:54:04 +0900

    調整

--- src/App.vue
+++ src/App.vue
@@ -5,19 +5,33 @@
     <h1 class="my-5 text-center">atara's PORTFOLIO</h1>
     <div class="container py-5 text-center border border-white rounded shadow card">
       <ul class="list-unstyled">
-        <li><i class="bi bi-twitter-x"></i> <a class="link-offset-1" href="https://x.com/atara_0327" target="_blank">@atara_0327</a></li>
-        <li><i class="bi bi-github"></i> <a class="link-offset-1" href="https://github.com/atara-327" target="_blank">atara-327</a></li>
+        <li>
+          <i class="bi bi-twitter-x"></i>
+          <a class="link-offset-1" href="https://x.com/atara_0327" target="_blank">@atara_0327</a>
+        </li>
+        <li>
+          <i class="bi bi-github"></i>
+          <a class="link-offset-1" href="https://github.com/atara-327" target="_blank">atara-327</a>
+        </li>
       </ul>
     </div>
     <br />
     <ul>
-      <li><a class="link-offset-1" href="https://sia-song.ataraman.com" target="_blank">依澄しあ 歌枠検索</a></li>
+      <li>
+        <a class="link-offset-1" href="https://sia-song.ataraman.com" target="_blank"
+          >依澄しあ 歌枠検索</a
+        >
+      </li>
     </ul>
-</div>
+  </div>
 </template>
 
 <style scoped>
 .card {
-  background-color: rgb(67, 40, 116);
+  background-color: inherit;
+}
+.bi {
+  color: #eaeaea;
+  margin-right: 0.5rem;
 }
 </style>
